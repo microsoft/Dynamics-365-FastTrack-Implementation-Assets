2850a0181d790cd15978598d4bfb5e8c7c87f4e3
Andreas Hofmann <45279749+andreashofmann1@users.noreply.github.com>
2020-06-27 14:23:12 -0700

    Commerce/NegativeInventoryCheck source code

---
+++ Commerce/NegativeInventoryCheck/Crt.NegativeInventoryCheck/ResumeCartRequestTrigger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace Extensions.Crt.NegativeInventoryCheck
+{
+    class ResumeCartRequestTrigger : IRequestTrigger
+    {
+        IEnumerable<Type> ISupportedTypesAware.SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(ResumeCartRequest) };
+            }
+        }
+
+        public void OnExecuted(Request request, Response response)
+        {
+            // no operation
+        }
+
+        public void OnExecuting(Request request)
+        {
+            // when we resume a transaction, we do not check (we will check during checkout)
+            request.RequestContext.SetProperty("SKIP_INVENTORY_CHECK", true);
+        }
+    }
+}
---
+++ Commerce/NegativeInventoryCheck/Crt.NegativeInventoryCheck/SaveCartVersionedDataRequestTrigger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions.Crt.NegativeInventoryCheck
+{
+    class SaveCartVersionedDataRequestTrigger : IRequestTrigger
+    {
+        IEnumerable<Type> ISupportedTypesAware.SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(SaveCartVersionedDataRequest) };
+            }
+        }
+
+        public void OnExecuted(Request request, Response response)
+        {
+            // no operation
+        }
+
+        public void OnExecuting(Request request)
+        {
+            SaveCartVersionedDataRequest r = (SaveCartVersionedDataRequest)request;
+
+            if (r.SalesTransaction != null && r.SalesTransaction.ActiveSalesLines.Count > 0)
+            {
+                // active lines - voided lines, aggregate them over ProductId
+                var productQuanityPairs = r.SalesTransaction.ActiveSalesLines.Where(asl => !asl.IsVoided).GroupBy(cl => new { cl.ProductId }).Select(group => new Tuple<long, decimal>(group.Key.ProductId, group.Sum(i => i.Quantity)));
+
+                InventoryRequestHelper.ValidateOnHandQuantities(productQuanityPairs, request.RequestContext);
+            }
+        }
+    }
+}
+
