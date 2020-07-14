5f6c7b610969bf3bba28f9c36fd28126201e128a
JJ Yadav <65608469+jiyada@users.noreply.github.com>
2020-07-14 22:54:26 +0000

    Add files via upload

---
+++ Analytics/AzureDataFactoryARMTemplates/SQLToADLSFullExport/ModelJsonToViewDefinition.sql
@@ -0,0 +1,50 @@
+DECLARE @json NVARCHAR(MAX);
+Declare @DataSource nvarchar(50);
+SET @json = N'Replace this with model.json data';
+set @DataSource  = N'SqlOnDemandDemo'; -- replace this with the name of datasource to connect storage account 
+
+declare @ViewDefinition varchar(max)
+
+select  @ViewDefinition = COALESCE(@ViewDefinition + ',', '') + fieldName 
++ ' ' + 
+case  dataType
+when 'int64'  then 'bigint'
+when 'int32' then  'int'
+when 'dateTime'  then  'varchar(100)'
+when 'decimal' then 'decimal'
+when 'double' then 'float'
+when 'boolean'  then 'bit'
+when 'string' then 'varchar(800)'
+end + ' '
+FROM OPENJSON(@json)
+  WITH (
+     entities NVARCHAR(MAX) '$.entities' AS JSON
+  )
+  OUTER APPLY OPENJSON(entities)
+  WITH 
+  ( attributes NVARCHAR(MAX) '$.attributes' AS JSON,
+  partitions NVARCHAR(MAX) '$.partitions' AS JSON
+  )
+   OUTER APPLY OPENJSON(attributes)
+  WITH (fieldName nvarchar(100) '$.name',
+        dataType nvarchar(50) '$.dataType')
+
+ 
+select 
+@ViewDefinition = 'CREATE or ALTER VIEW ' + entityName 
++' AS SELECT * FROM OPENROWSET(BULK '''+ location + ''',  DATA_SOURCE = '+ @SqlOnDemandDemo + ', FORMAT = ''CSV'', Parser_Version= ''2.0'') 
+WITH (' + @ViewDefinition + ') as r'
+FROM OPENJSON(@json)
+  WITH (
+     entities NVARCHAR(MAX) '$.entities' AS JSON
+  )
+  OUTER APPLY OPENJSON(entities)
+  WITH 
+  ( 
+    entityName nvarchar(100) '$.name',
+    partitions NVARCHAR(MAX) '$.partitions' AS JSON
+  )
+  OUTER APPLY OPENJSON(partitions)
+  WITH (location nvarchar(1000) '$.location') ;
+
+   select @ViewDefinition as ViewDefinition
