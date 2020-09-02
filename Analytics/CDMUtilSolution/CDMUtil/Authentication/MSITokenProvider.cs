a0fa1d19b22936a539c73baac2b54b218a395d5b
JJ <jiyadav@microsoft.com>
2020-09-02 16:24:27 +0000

    Updated Authentication models

---
+++ Analytics/CDMUtilSolution/CDMUtil/Authentication/MSITokenProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.CommonDataModel.ObjectModel.Utilities.Network;
+
+namespace CDMUtil.Manifest
+{
+    public class MSITokenProvider : TokenProvider
+    {
+        string Resource;
+        public MSITokenProvider(string resource)
+        {
+            Resource = resource;
+        }
+        public string GetToken()
+            {
+                Console.WriteLine("gettting MNSI token");
+                string token = this.GetAccessTokenAsync().Result;
+                return $"Bearer {token}";
+            }
+            private async Task<string> GetAccessTokenAsync()
+            {
+                var tokenProvider = new AzureServiceTokenProvider();
+                string token = await tokenProvider.GetAccessTokenAsync(Resource);
+
+                return token;
+            }
+        }
+}
