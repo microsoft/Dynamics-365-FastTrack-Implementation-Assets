3e01b57e603ab0a09e0f5af4b2b04e76d454118d
JJ Yadav <65608469+jiyada@users.noreply.github.com>
2020-09-15 09:17:06 -0500

    Add files via upload

---
+++ Analytics/AzureDataFactoryARMTemplates/SQLToADLSFullExport/CreateAADUser.sql
@@ -0,0 +1,16 @@
+-- Use bellow steps to add AAD users to SQL-On-Demand. In order to query data AAD users would also need Blob data reader access in Azure data lake
+use master
+go
+--Step 1-  create AAD Login
+CREATE LOGIN [jiyada@microsoft.com] FROM EXTERNAL PROVIDER;
+
+-- Step 2 
+	-- Option 1 - Add server level so to grant access to all databases 
+	ALTER SERVER ROLE  sysadmin  ADD MEMBER [jiyada@microsoft.com];
+
+	--Option 2 - Add database level access to 
+	use AXDBLake -- Use your DB name
+	go
+	CREATE USER jiyada FROM LOGIN [jiyada@microsoft.com];
+	alter role db_owner Add member jiyada -- Type USER name from step 2
+
---
+++ Analytics/AzureDataFactoryARMTemplates/SQLToADLSFullExport/SynapseCreateUser.sql
@@ -0,0 +1,29 @@
+﻿-- Follow the steps to create SQL user for SQL-On-Demand endpoint. To query data stored in Azure data lake via SQL-On-Demand using SQL user security context
+-- You will also need to create credentials and grant reference or create view or extenral tables using datasource 
+
+/*****************Option 1 - to create and grant server level access do following***********************/ 
+
+--Step 1 -- create a sql user 
+use master; 
+CREATE LOGIN [serverUser] WITH PASSWORD = 'Password'; 
+
+-- Step 2  --
+ALTER SERVER ROLE  sysadmin  ADD MEMBER [newuser];
+
+
+/*****************Option 2 - To grant database level access do following***********************/ 
+
+-- Step 1 - create login 
+CREATE LOGIN [dbUser] WITH PASSWORD = 'PassWord'; 
+-- Step 2 - create user and add roles 
+USE [AXDWLake] 
+CREATE USER [dbUser] FOR LOGIN [dbUser] 
+ALTER ROLE db_owner ADD MEMBER [dbUser]
+
+-- Step 3 -- Create credential for the root of storage account with Shared access key( needed only one time) validate if credential already exists using select * from Sys.Credentials
+CREATE CREDENTIAL [https://lakestorage.dfs.core.windows.net] With identity ='SHARED ACCESS SIGNATURE', 
+SECRET = 'sv=2019-10-10&ssssssssssssssssssssssssssssr=https&sig=a6%s%3D' 
+
+--Step 4 -- Grant user reference on credentials 
+use master
+GRANT REFERENCES ON CREDENTIAL::[https://lakestorage.dfs.core.windows.net] TO [dbUser]
