c0862c97ab34db4c6827ca36ff1a9aab5f0fce4c
jiyada <jiyada@microsoft.com>
2021-03-01 09:42:18 -0600

    1. Added $fileName as column, Fixed Date conversion, fixed external table syntax, added option to generate statement for given table

--- Analytics/CDMUtilSolution/CDMUtil/Manifest/ManifestHandler.cs
+++ Analytics/CDMUtilSolution/CDMUtil/Manifest/ManifestHandler.cs
@@ -430,7 +430,7 @@ namespace CDMUtil.Manifest
         }
 
         
-        public async static Task<List<SQLStatement>> SQLMetadataToDDL(List<SQLMetadata> metadataList, string type, string schema="dbo", string fileFormat ="", string dataSourceName="")
+        public async static Task<List<SQLStatement>> SQLMetadataToDDL(List<SQLMetadata> metadataList, string type, string schema="dbo", string fileFormat ="", string dataSourceName="", string tableNames="")
         {
             List<SQLStatement> sqlStatements = new List<SQLStatement>();
             string template ="";
@@ -439,23 +439,33 @@ namespace CDMUtil.Manifest
             {
                // {0} Schema, {1} TableName, {2} ColumnDefinition {3} data location ,{4} DataSource, {5} FileFormat
                 case "SynapseView":
-                    template = @"CREATE OR ALTER VIEW {0}.{1} AS SELECT {6} FROM OPENROWSET(BULK '{3}', FORMAT = 'CSV', PARSER_VERSION = '2.0', DATA_SOURCE ='{4}') WITH ({2}) as r";
+                    template = @"CREATE OR ALTER VIEW {0}.{1} AS SELECT r.filepath(1) as [$FileName], {6} FROM OPENROWSET(BULK '{3}', FORMAT = 'CSV', PARSER_VERSION = '2.0', DATA_SOURCE ='{4}') WITH ({2}) as r";
                     break;
                 case "SQLTable":
                     template = @"CREATE Table {0}.{1} ({2})";
                     break;
 
                 case "SynapseExternalTable":
-                    template = @"If (OBJECT_ID('{0}.{1}') is not NULL)   drop external table  {0}.{1} ;  create   EXTERNAL TABLE {0}.{1} ({2}) WITH (LOCATION = '{3}', DATA_SOURCE ={4}, FILE_FORMAT = {5}, PARSER_VERSION = '2.0')";
+                    template = @"If (OBJECT_ID('{0}.{1}') is not NULL)   drop external table  {0}.{1} ;  create   EXTERNAL TABLE {0}.{1} ({2}) WITH (LOCATION = '{3}', DATA_SOURCE ={4}, FILE_FORMAT = {5})";
                     break;
 
             }
-            
             foreach (SQLMetadata metadata in metadataList)
             {
-                var sql = string.Format(template,schema, metadata.entityName, metadata.columnDefinition, metadata.dataLocation,dataSourceName,fileFormat, metadata.columnNames);
-          
-                sqlStatements.Add(new SQLStatement() { Statement = sql });
+                if (String.IsNullOrEmpty(tableNames))
+                {
+                    var sql = string.Format(template, schema, metadata.entityName, metadata.columnDefinition, metadata.dataLocation, dataSourceName, fileFormat, metadata.columnNames);
+                    sqlStatements.Add(new SQLStatement() { Statement = sql });
+                }
+                else
+                {
+                    var filteredTables = tableNames.Split(',');
+                    if (filteredTables.Contains(metadata.entityName))
+                    {
+                        var sql = string.Format(template, schema, metadata.entityName, metadata.columnDefinition, metadata.dataLocation, dataSourceName, fileFormat, metadata.columnNames);
+                        sqlStatements.Add(new SQLStatement() { Statement = sql });
+                    }
+                }
             }
             
             return sqlStatements;
@@ -533,7 +543,8 @@ namespace CDMUtil.Manifest
                 {
                     dataLocation = $"{localRoot}/{entityName}/*.*";
                 }
-                
+
+                dataLocation = dataLocation.Replace("//", "/");
 
                 string fileName = dataLocation.Substring(dataLocation.LastIndexOf("/") + 1);
                 string ext = fileName.Substring(fileName.LastIndexOf("."));
@@ -541,7 +552,8 @@ namespace CDMUtil.Manifest
 
                 var entSelected = await manifestHandler.cdmCorpus.FetchObjectAsync<CdmEntityDefinition>(eDef.EntityPath, manifest);
                 string columnDef = string.Join(", ", entSelected.Attributes.Select(i => CdmTypeToSQl((CdmTypeAttributeDefinition)i))); 
-                string columnNames = string.Join(", ", entSelected.Attributes.Select(i => CdmAttributeToColumnNames((CdmTypeAttributeDefinition)i,convertDateTime))); 
+                string columnNames = string.Join(", ", entSelected.Attributes.Select(i => CdmAttributeToColumnNames((CdmTypeAttributeDefinition)i,convertDateTime)));
+              
                 metadataList.Add(new SQLMetadata() { entityName = entityName, columnDefinition = columnDef, dataLocation = dataLocation, columnNames = columnNames });
             }
             return true;
@@ -717,6 +729,7 @@ namespace CDMUtil.Manifest
             string sqlColumnDef;
             string dataType;
 
+            
             if (typeAttributeDefinition.DataType == null)
             {
                 dataType = typeAttributeDefinition.DataFormat.ToString().ToLower();
@@ -726,7 +739,16 @@ namespace CDMUtil.Manifest
                 dataType = typeAttributeDefinition.DataType.NamedReference.ToLower();
             }
 
-            int maximumLenght = DefaultMaxLength;
+            int maximumLenght;
+
+            if (typeAttributeDefinition.Name.ToLower() == "dataareaid")
+            {
+                maximumLenght = 5;
+            }
+            else
+            {
+                maximumLenght = DefaultMaxLength;
+            }
 
             string decimalPrecisionScale = $"({DefaultPrecison}, {DefaultScale})";
 
@@ -797,7 +819,7 @@ namespace CDMUtil.Manifest
                 case "datetime2":
                     if (convertDatetime)
                     {
-                        sqlColumnNames = $" TRY_CONVERT(DATETIME, {typeAttributeDefinition.Name}, 102) as {typeAttributeDefinition.Name}";
+                        sqlColumnNames = $" TRY_CONVERT(DATETIME2, {typeAttributeDefinition.Name}, 102) as {typeAttributeDefinition.Name}";
                     }
                     else
                     {
