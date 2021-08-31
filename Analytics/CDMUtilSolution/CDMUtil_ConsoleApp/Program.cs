6baab706236690f47a763539ed53e5bf485e1c0e
jiyada <jiyada@microsoft.com>
2021-08-31 06:33:22 -0500

    Refactored code, Simplified paramaters, replacing view and handling metadata exceptions, EventGrid trigger

--- Analytics/CDMUtilSolution/CDMUtil_ConsoleApp/Program.cs
+++ Analytics/CDMUtilSolution/CDMUtil_ConsoleApp/Program.cs
@@ -8,98 +8,121 @@ using System.Configuration;
 using System.Collections.Specialized;
 using System.IO;
 
-namespace ManifestToSQLView
+namespace ManifestToSQL
 {
     class Program
     {
         static void Main(string[] args)
-        {      
-            //get data from config 
-            string tenantId             = ConfigurationManager.AppSettings.Get("TenantId");//"979fd422-22c4-4a36-bea6-1cf87b6502dd";
-            string storageAccount       = ConfigurationManager.AppSettings.Get("StorageAccount");//"ftfinanced365fo.dfs.core.windows.net";
-            string accessKey            = ConfigurationManager.AppSettings.Get("AccessKey");
-            string rootFolder           = ConfigurationManager.AppSettings.Get("RootFolder");//"/dynamics365-financeandoperations/finance.sandbox.operations.dynamics.com/";
-            string manifestFilePath     = ConfigurationManager.AppSettings.Get("ManifestFilePath");
-            var targetDbConnectionString= ConfigurationManager.AppSettings.Get("TargetDbConnectionString");//"Server=ftsasynapseworkspace-ondemand.sql.azuresynapse.net;Database=Finance_AXDB";
-            string dataSourceName       = ConfigurationManager.AppSettings.Get("DataSourceName");//"finance" ;
-            string DDLType              = ConfigurationManager.AppSettings.Get("DDLType");//"SynapseExternalTable";
-            string schema               = ConfigurationManager.AppSettings.Get("Schema");//"ChangeFeed";
-            string fileFormat           = ConfigurationManager.AppSettings.Get("FileFormat"); //"CSV";
-            string DateTimeAsString = ConfigurationManager.AppSettings.Get("DateTimeAsString"); //"CSV";
-            string ConvertDateTime = ConfigurationManager.AppSettings.Get("ConvertDateTime"); //"CSV";
-            string TableNames =     ConfigurationManager.AppSettings.Get("TableNames"); //"CSV";
-
-
-            NameValueCollection sAll = ConfigurationManager.AppSettings;
-            foreach (string s in sAll.AllKeys)
-                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
-
-            if (String.IsNullOrEmpty(manifestFilePath))
-            {
-                Console.WriteLine("Enter Manifest file relative path:(/Tables/Tables.manifest.cdm.json or /Tables/model.json)");
-                manifestFilePath = Console.ReadLine();
-            }
-            
-            string manifestName = Path.GetFileName(manifestFilePath);
-            string localFolder = manifestFilePath.Replace(manifestName, "");
-            bool MSIAuth;
-            
-            if (String.IsNullOrEmpty(accessKey))
-            {
-                MSIAuth = true;
-            }
-            else
-            {
-                MSIAuth = false;
-            }
-
-            AdlsContext adlsContext = new AdlsContext()
-            {
-                StorageAccount = storageAccount,
-                FileSytemName = rootFolder,
-                MSIAuth = MSIAuth,
-                TenantId = tenantId,
-                SharedKey = accessKey
-            };
+        {
+            AppConfigurations c = loadConfigurations();
 
             // Read Manifest metadata
-            Console.WriteLine($"Reading Manifest metadata https://{storageAccount}{rootFolder}{manifestFilePath}" );
-
+            Console.WriteLine($"Reading Manifest metadata https://{c.AdlsContext.StorageAccount}{c.rootFolder}{c.manifestName}");
             List<SQLMetadata> metadataList = new List<SQLMetadata>();
 
-            bool convertDateTime = ConvertDateTime.ToLower() == "true" ? true : false;
-            bool dateTimeAsString = DateTimeAsString.ToLower() == "true" ? true : false;
-            List<string> tableList =  String.IsNullOrEmpty(TableNames) ? new List<string>(){ "*" } : new List<string>(TableNames.Split(','));
+            ManifestReader.manifestToSQLMetadata(c, metadataList).Wait();
 
-            ManifestReader.manifestToSQLMetadata(adlsContext, manifestName, localFolder, metadataList, dateTimeAsString, convertDateTime, tableList);
-
-          
             // convert metadata to DDL
             Console.WriteLine("Converting metadata to DDL");
-            var statementsList = SQLHandler.SQLMetadataToDDL(metadataList, DDLType,schema,fileFormat, dataSourceName);
+            var statementsList = SQLHandler.SQLMetadataToDDL(metadataList, c);
+
+            Console.WriteLine($"Count of entities:{metadataList.Count}");
+
+            Console.WriteLine("Preparing DB");
+            // prep DB 
+            if (c.synapseOptions.targetDbConnectionString != null)
+            {
+                 SQLHandler.dbSetup(c.synapseOptions, c.tenantId);
+            }
 
             // Execute DDL
             Console.WriteLine("Executing DDL");
-            SQLHandler sQLHandler = new SQLHandler(targetDbConnectionString, tenantId);
-            SQLStatements statements = new SQLStatements { Statements = statementsList.Result};
+            SQLStatements statements = new SQLStatements { Statements = statementsList.Result };
+            
             try
             {
+                SQLHandler sQLHandler = new SQLHandler(c.synapseOptions.targetDbConnectionString, c.tenantId);
                 sQLHandler.executeStatements(statements);
                 foreach (var statement in statements.Statements)
                 {
                     Console.WriteLine(statement.Statement);
                     Console.WriteLine("Status:" + statement.Created);
                     Console.WriteLine("Detail:" + statement.Detail);
-
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("ERROR executing SQL");
+                foreach (var statement in statements.Statements)
+                {
+                    Console.WriteLine(statement.Statement);
+                }
                 Console.WriteLine(e.Message);
             }
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
+        }
+
+        static AppConfigurations loadConfigurations()
+        {
+            //get data from config 
+            string tenantId = ConfigurationManager.AppSettings.Get("TenantId");
+            string ManifestURL = ConfigurationManager.AppSettings.Get("ManifestURL");
+            string accessKey = ConfigurationManager.AppSettings.Get("AccessKey");
+            string DDLType = ConfigurationManager.AppSettings.Get("DDLType");
+            string targetDbConnectionString = ConfigurationManager.AppSettings.Get("TargetDbConnectionString");
+           
+            NameValueCollection sAll = ConfigurationManager.AppSettings;
+            foreach (string s in sAll.AllKeys)
+            {
+                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+            }
+
+            AppConfigurations AppConfiguration = new AppConfigurations(tenantId, ManifestURL, accessKey, targetDbConnectionString, DDLType);
+
+            //Optional parameters overide 
+            if (AppConfiguration.tableList == null)
+            {
+                string TableNames = ConfigurationManager.AppSettings.Get("TableNames");
+                AppConfiguration.tableList = String.IsNullOrEmpty(TableNames) ? new List<string>() { "*" } : new List<string>(TableNames.Split(','));
+            }
+            
+            if (ConfigurationManager.AppSettings.Get("TranslateEnum") != null)
+            {
+                AppConfiguration.synapseOptions.TranslateEnum = bool.Parse(ConfigurationManager.AppSettings.Get("TranslateEnum"));
+            }
+
+            if (ConfigurationManager.AppSettings.Get("DateTimeAsString") != null)
+            {
+                AppConfiguration.synapseOptions.DateTimeAsString = bool.Parse(ConfigurationManager.AppSettings.Get("DateTimeAsString"));
+            }
+
+            if (ConfigurationManager.AppSettings.Get("ConvertDateTime") != null)
+            {
+                AppConfiguration.synapseOptions.DateTimeAsString = bool.Parse(ConfigurationManager.AppSettings.Get("ConvertDateTime"));
+            }
+            if (ConfigurationManager.AppSettings.Get("DataSourceName") != null)
+            {
+                AppConfiguration.synapseOptions.external_data_source = ConfigurationManager.AppSettings.Get("DataSourceName");
+            }
+            if (ConfigurationManager.AppSettings.Get("Schema") != null)
+            {
+                AppConfiguration.synapseOptions.schema = ConfigurationManager.AppSettings.Get("Schema");
+            }
+            if (ConfigurationManager.AppSettings.Get("FileFormat") != null)
+            {
+                AppConfiguration.synapseOptions.fileFormatName = ConfigurationManager.AppSettings.Get("FileFormat");
+            }
+            if (ConfigurationManager.AppSettings.Get("AXDBConnectionString") != null)
+            {
+                AppConfiguration.AXDBConnectionString = ConfigurationManager.AppSettings.Get("AXDBConnectionString");
+            }
+            
+            AppConfiguration.SourceColumnProperties = Path.Combine(Environment.CurrentDirectory, "Manifest", "SourceColumnProperties.json");
+            AppConfiguration.ReplaceViewSyntax = Path.Combine(Environment.CurrentDirectory, "SQL", "ReplaceViewSyntax.json");
+            
+           
+            return AppConfiguration;
         }
     }
 }
