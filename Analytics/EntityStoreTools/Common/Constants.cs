5e9e40973a3a985399a45077a195db32264b431d
Arvind Vidyashankar <arvidyas@microsoft.com>
2021-11-07 19:26:01 -0800

    Entity Store Tools 2.7

---
+++ Analytics/EntityStoreTools/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Constants
+    {
+        public const string ToolsVersion = "2.7";
+    }
+}
