d33250c64c567e5cff6ba53ce7da02d1afc84adf
avanthi710 <134454354+avanthi710@users.noreply.github.com>
2023-06-01 13:45:17 -0500

    Update AutomationScript.ps1

--- Customer Service/Power-BI/ALM/AutomationScript.ps1
+++ Customer Service/Power-BI/ALM/AutomationScript.ps1
@@ -1,4 +1,13 @@
-﻿function Get-PBIAccessToken {
+// =====================================================================
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+//
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+// =====================================================================
+function Get-PBIAccessToken {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
