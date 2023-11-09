e97247b7a05d10375eefc0409b3517fd8515234a
jiyada <jiyada@microsoft.com>
2023-11-09 07:21:21 -0600

    Updated documentation and scipt for Synapse Link DV Transition Guidance

---
+++ Analytics/DataverseLink/EDL_To_SynapseLinkDV_DBSetup/Step0_EDL_To_SynapseLinkDV_CreateSourceDatabaseAndMasterKey.sql
@@ -0,0 +1,16 @@
+--STEP 1: Create a new database in Synapse serverless 
+-- TODO: UPDATE Database as needed, 
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'YourNewDataBaseName')
+	create database [YourNewDataBaseName]
+
+-- STEP 2: Switch to new database created
+use [YourNewDataBaseName]
+
+--Step 3 : Create Master Key  
+-- Create Marker KEY Encryption if not exist - this is required to create database scope credentials 
+-- You may choose to create own encryption key instead of generating random as done in script bellow
+	DECLARE @randomWord VARCHAR(64) = NEWID();
+	DECLARE @createMasterKey NVARCHAR(500) = N'
+	IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name = ''##MS_DatabaseMasterKey##'')
+		CREATE MASTER KEY ENCRYPTION BY PASSWORD = '  + QUOTENAME(@randomWord, '''')
+	EXECUTE sp_executesql @createMasterKey
