2dc0c94da88d7d2379a59c4916d62e2d88b82c67
SamySid <52775403+SamySid@users.noreply.github.com>
2025-04-08 02:51:12 +0200

    Update 0-PreRequisits.sql

--- Storage Management/Large Tables Data Cleanup/0-PreRequisits.sql
+++ Storage Management/Large Tables Data Cleanup/0-PreRequisits.sql
@@ -1,9 +1,20 @@
-drop table dbo.FTInformationcolums
-SELECT * into FTInformationcolums FROM (
-select isc.*, sdf.TABLEID from [INFORMATION_SCHEMA].[COLUMNS] isc
-inner join
-(select SQLName,TABLEID from SQLDICTIONARY sd where sd.FIELDID = 0 and sd.ARRAY = 0) sdf on isc.Table_Name = sdf.SQLNAME
-		inner join TABLEMETADATATABLE TMT ON TMT.TABLEID = sdf.TABLEID
-		where (TMT.TABLEGROUP IN (4, 5, 6, 9, 10, 11) or SDF.SQLNAME in ('WHSASNITEM', 'WHSASNITEMRECEIPT', 'WHSUOMSTRUCTURE')) 
-		and data_type =  'datetime'
-  ) selection
+-- Drop the table FTInformationcolums if it exists
+DROP TABLE IF EXISTS dbo.FTInformationcolums;
+
+-- Create a new table FTInformationcolums and insert data into it
+SELECT * INTO FTInformationcolums FROM (
+    -- Select all columns from INFORMATION_SCHEMA.COLUMNS
+    SELECT isc.*, sdf.TABLEID 
+    FROM [INFORMATION_SCHEMA].[COLUMNS] isc
+    -- Inner join with SQLDICTIONARY to get TABLEID where FIELDID = 0 and ARRAY = 0
+    INNER JOIN (
+        SELECT SQLName, TABLEID 
+        FROM SQLDICTIONARY sd 
+        WHERE sd.FIELDID = 0 AND sd.ARRAY = 0
+    ) sdf ON isc.Table_Name = sdf.SQLNAME
+    -- Inner join with TABLEMETADATATABLE to get metadata information
+    INNER JOIN TABLEMETADATATABLE TMT ON TMT.TABLEID = sdf.TABLEID
+    -- Filter the results based on TABLEGROUP and SQLNAME, and data_type = 'datetime'
+    WHERE (TMT.TABLEGROUP IN (4, 5, 6, 9, 10, 11) OR SDF.SQLNAME IN ('WHSASNITEM', 'WHSASNITEMRECEIPT', 'WHSUOMSTRUCTURE')) 
+    AND data_type = 'datetime'
+) selection;
